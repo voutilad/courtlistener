606d26549c48ca14ba50411671a9f2e5d7a8f442
Dave Voutila <voutilad@gmail.com>
2016-01-04 16:41:34 -0500

    added test case for calling size attribute on the local_path_mp3 field for Audio models

--- cl/tests/test_fixtures.py
+++ cl/tests/test_fixtures.py
@@ -1,8 +1,12 @@
 """
 Unit tests for validating Fixture loading works with model logic
 """
+from django.conf import settings
 from django.test import TestCase
+from django.test.utils import override_settings
+from cl.audio.models import Audio
 from cl.judges.models import Judge
+import os
 
 class FixtureTest(TestCase):
     """Used to validate certain aspects of various fixture files...
@@ -15,3 +19,24 @@ class FixtureTest(TestCase):
         judge = Judge.objects.get(pk=1)
         self.assertEqual(judge.name_first, 'Judith')
         self.assertEqual(judge.name_last, 'Sheindlin')
+
+
+@override_settings(
+    MEDIA_ROOT = os.path.join(settings.INSTALL_ROOT, 'cl/assets/media/test/')
+)
+class AudioFixtureTest(TestCase):
+
+    fixtures = ['test_court.json', 'judge_judy.json', 'functest_opinions.json',
+        'functest_audio.json']
+
+    def test_calling_size_attr_on_audio_file(self):
+        audio = Audio.objects.get(pk=10)
+        # self.file_size_mp3 = deepgetattr(item, 'local_path_mp3.size', None)
+        local_path_mp3 = audio.local_path_mp3
+        print 'MEDIA_ROOT: %s' % (settings.MEDIA_ROOT,)
+        print 'path: %s' % (getattr(local_path_mp3, 'path'))
+
+        try:
+            print 'size: %s' % (getattr(local_path_mp3, 'size'))
+        except OSError:
+            self.fail('size attribute cannot be called on local_path_mp3')
